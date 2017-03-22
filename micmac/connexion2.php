ed863a649c13ff49e10e6cd6246e440db7cc5b5d
SafaFennia <safafennia@gmail.com>
2017-03-22 14:15:03 +0100

    Add files via upload

--- micmac/connexion2.php
+++ micmac/connexion2.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -10,20 +13,26 @@
   $link = mysqli_connect("127.0.0.1", "root", "","micmac");
   if(isset($_POST['email']) && isset($_POST['passe'])){
 
-
   $mail = mysqli_real_escape_string($link,htmlspecialchars($_POST['email']));
   $passe = $_POST['passe'];
   $passe = sha1($passe);
+
+  //requete
   $requete = "SELECT * FROM users  WHERE mail='$mail'";
   if($result = mysqli_query($link, $requete)) {
-    echo "connexion et requete ok ";
+
     while($ligne = mysqli_fetch_assoc($result)) {
-      $etud[]=$ligne;
       $id_user = $ligne["id_user"];
       $pseudo = $ligne["pseudo"];
       $mdp = $ligne["mdp"];
       $email = $ligne["mail"];
-      echo "id ".$id_user." pseudo ".$pseudo." mdp ".$mdp." email ".$email;
+
+        // Set session variables
+      $_SESSION["pseudo"] = $pseudo;
+      $_SESSION["email"] = $email;
+      $_SESSION["mdp"] = $mdp;
+      print_r($_SESSION);
+
 
   }
 }
