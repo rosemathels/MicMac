bf31af9306a83fa6c1e37e6e7b9440c73c933c67
mharrach <harrach.mouna@hotmail.com>
2017-04-29 18:12:40 +0200

    Add files via upload

---
+++ ProjetDev/UserPage/UploadPicture2.php
@@ -0,0 +1,128 @@
+<?php
+session_start();
+
+$conn = mysqli_connect("127.0.0.1", "root", "","micmac");
+
+if (!$conn) {
+  echo "Erreur de connexion à la BDD.";
+  exit;
+}
+
+//Récupération des données du formulaire
+$nom_chantier = mysqli_real_escape_string($conn,$_POST["nom_chantier"]);
+$type_cam = mysqli_real_escape_string($conn,$_POST["type_camera"]);
+
+//Création du dossier contenant les images (nom du dossier = nom du chantier)
+$nom_chantier = $_POST["nom_chantier"];
+$target_dir = "uploads/".$nom_chantier."/";
+mkdir($target_dir);
+
+//Récupération du nb d'images uploadées
+$nb_images = count($_FILES["myimage"]["name"]);
+
+//Récupération des informations de l'image
+$target_file = $target_dir . basename($_FILES["myimage"]["name"][0]); //récupération du nom de la 1ère image
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION); //récupération de l'extension de l'image
+$imageSize = getimagesize($_FILES["myimage"]["tmp_name"][0]); //récupération de la taille de l'image
+//on considère que toutes les images ont le même format et la même taille
+
+$upload_failed = false;
+
+//Parcours des images
+for($i = 0; $i < $nb_images; $i++){
+
+  //Récupération du nom des images
+  $current_image = $target_dir . basename($_FILES["myimage"]["name"][$i]);
+  $currentFileType = pathinfo($current_image,PATHINFO_EXTENSION); //récupération de l'extension de l'image
+
+  // Vérifier que les fichiers sont bien des images
+  if(isset($_POST["submit"])) {
+      $check = getimagesize($_FILES["myimage"]["tmp_name"][$i]);
+      if($check !== false) {
+          echo "File is an image - " . $check["mime"] . ".";
+          $uploadOk = 1;
+      } else {
+          echo "File is not an image.";
+          $uploadOk = 0;
+      }
+  }
+  
+  // Vérifier si les images existent déjà
+  if (file_exists($current_image)) {
+      echo "Sorry, file already exists.";
+      $uploadOk = 0;
+  }
+  // Vérifier la taille des images
+  if ($_FILES["myimage"]["size"][$i] > 209715200) {
+     echo "Sorry, your file is too large.";
+     $uploadOk = 0;
+  }
+  // Autoriser certaines formats des images
+  if($currentFileType != "jpg" && $currentFileType != "png" && $currentFileType != "jpeg" && $currentFileType != "gif" && $currentFileType != "JPG") {
+      echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+      $uploadOk = 0;
+  }
+
+  //Si tout est ok, on ajoute l'image dans le dossier
+  if($uploadOk ==1){
+    //upload
+    if(move_uploaded_file($_FILES["myimage"]["tmp_name"][$i], $current_image)){
+      //ajout des infos dans la BDD
+      $insert_path = "INSERT INTO images(chemin,nom_image) VALUES('$target_dir','$current_image')";
+      $var = mysqli_query($conn, $insert_path);
+    }
+    else{
+      $upload_failed = true; //cas où l'upload a échoué
+    }
+  }
+  else{
+    $upload_failed = true; //cas où les images sont invalides pour l'upload
+  }
+
+}
+
+//SI L'UPLOAD S'EST BIEN PASSE, ON REMPLIT LA BDD
+
+// Vérifier si l'upload a été bien fait
+if ($upload_failed) {
+    echo "Votre chantier n'est pas valide, veuillez recommencer";
+}
+else {
+  //Si tout est ok, on remplit la table chantier
+
+  //Calcul de la résolution
+  if($imageSize[0] >= $imageSize[1]){
+    $imageResolution = $imageSize[0];
+  }
+  else{
+    $imageResolution = $imageSize[1];
+  }
+
+  //Récupération données de session
+  $date = getdate();
+  $datestring = $date["mday"]."/".$date["mon"]."/".$date["year"];
+  $id_user = $_SESSION['id_user'];
+
+  //Insertion des infos du chantier dans la BDD
+  $requete_chantier = "INSERT INTO chantiers VALUES (NULL,'$nom_chantier','$datestring','$id_user','$type_cam','$imageResolution','$imageFileType', 0, 0, 'en_attente', '$target_dir')";
+  $result_chantier = mysqli_query($conn, $requete_chantier);
+
+  if (!$result_chantier) {
+    echo "Erreur : les informations du chantier n'ont pas pu être insérées dans la base de données.";
+  }
+  $id_chantier = mysqli_insert_id($conn);
+
+  //Insertions des instructions MicMac dans la BDD
+  include("instructions.php");
+  $nb_instr = setMicMacTable($id_chantier, $conn); //on récupère au passage le nombre d'instructions
+
+  //Ajout du nombre d'étapes dans la BDD (définie grâce à setMicMac)
+  $requete_update = "UPDATE chantiers SET nb_etapes = '$nb_instr' WHERE id_chantier = '$id_chantier'";
+  $result_update = mysqli_query($conn, $requete_update);
+  if (!$result_update) {
+    echo "Erreur : les informations du chantier n'ont pas pu être insérées dans la base de données.";
+  }
+
+}
+?>
---
+++ ProjetDev/UserPage/instructions.php
@@ -0,0 +1,97 @@
+<?php
+
+function setMicMacTable($id_chantier,$conn){
+  //fonction qui insère les instructions MicMac dans la table instructions_micmac dans la BDD
+
+  $requete_param = "SELECT type_cam, resolution, format_img from chantiers WHERE id_chantier =".$id_chantier;
+  $param = mysqli_query($conn, $requete_param);
+
+  $liste_instructions = setMicMacInstructions($param);
+  $ordre = 1;
+
+  foreach($liste_instructions as $instr){
+    $requete_instructions = "INSERT INTO instructions VALUES (NULL,'$id_chantier','$instr','$ordre')";
+    $result_instructions = mysqli_query($conn, $requete_instructions);
+    if (!$result_instructions) {
+      echo "Erreur : les instructions MicMac n'ont pas pu être insérées dans la base de données.";
+      exit;
+    }
+    $ordre++;
+  };
+
+  $nb_instr = sizeof($liste_instructions);
+
+  return $nb_instr;
+
+}
+
+function setMicMacInstructions($param){
+  //fonction qui génère la liste ordonnée des instructions MicMac à effectuer en fonction des paramètres du
+
+  //Récupération des paramètres
+  foreach ($param as $para) {
+    $type_cam = $para["type_cam"];
+    $resolution = $para["resolution"];
+    $format_img = $para["format_img"];
+  }
+
+  //Détermination du modèle de distorsion en fonction du type de la caméra
+  if($type_cam == "classique"){
+    $modele_distorsion = "RadialStd";
+  }
+  else if($type_cam == "fisheye"){
+    $modele_distorsion = "FishEyeEqui";
+  }
+
+  //Initialisation de la liste des instructions
+  $liste_instructions = [];
+
+  //Extraction des coordonnées GPS dans les exif
+  //Sortie => GpsCoordinatesFromExif.txt
+  $XifGps2Txt = 'mm3d XifGps2Txt ".*'.$format_img.'" Sys=GeoC';
+  array_push($liste_instructions, $XifGps2Txt);
+
+  //Conversion des coordonnées en L93, en dossier d'orientation MicMac pour faire la bascule ensuite, et en fichier xml pour les points de liaison
+  //Sortie => FileGPS.xml
+  $OriConvert = 'mm3d OriConvert "#F=N_X_Y_Z" GpsCoordinatesFromExif.txt GPS_L93 NameCple=FileGPS.xml ChSys=GeoC@Lambert93';
+  array_push($liste_instructions, $OriConvert);
+
+  //Recherche des points de liaisons
+  //Sortie => Homol => PastisImagename => fichiers binaires(.dat) contenant les points de liaison
+  $Tapioca = 'mm3d Tapioca FileGPS.xml '.$resolution;
+  array_push($liste_instructions, $Tapioca);
+
+  //Mise en place relative et calibration
+  //Sortie => Rel
+  $Tapas = 'mm3d Tapas '.$modele_distorsion.' ".*'.$format_img.'" Out=Rel';
+  array_push($liste_instructions, $Tapas);
+
+  //Basculement rigide à partir de la mise en place relative pour s'appuyer sur les somemts GPS
+  //Sortie =>
+  $CenterBascule = 'mm3d CenterBascule ".*'.$format_img.'" Rel GPS_L93 Bascule_L93';
+  array_push($liste_instructions, $CenterBascule);
+
+  //si GPS précis sur la phase...
+  //$Campari = 'mm3d Campari...'
+  //array_push($liste_instructions, $Campari);
+
+  //Reconstruction 3D - éventuellement donner le choix à l'utilisateur entre MicMac et les deux autres...
+  //Sortie => ?
+  $C3DC = 'mm3d C3DC MicMac ".*'.$format_img.'" Bascule_L93';
+  array_push($liste_instructions, $C3DC);
+
+  //Conversion en grille 2.5D (MNT, carte de profondeur) + orthorectification des images individuellement
+  //Sortie => ?
+  $Pims2Mnt = 'mm3d Pims2Mnt MicMac DoOrtho=1';
+  array_push($liste_instructions, $Pims2Mnt);
+
+  //Assemblage des orthos individuelles - éventuellement donner le choix à l'utilisateur entre Deq=0, RadoimEgal=0, .....
+  //Sortie => ?
+  $Tawny = 'mm3d Tawny PIMs-ORTHO/ Deq=0';
+  array_push($liste_instructions, $Tawny);
+
+  return $liste_instructions;
+
+}
+
+?>
