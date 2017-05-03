14e6b5d9944b90564c52ba3825033ad84e8e6973
RoseMathelier <rose.mathelier@gmail.com>
2017-05-03 10:54:48 +0200

    Affichage des chantiers en cours

--- mesChantiers.js
+++ mesChantiers.js
@@ -7,35 +7,65 @@ function getInfosChantier() {
 	ajax.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
 
-      //On récupère les infos du chantier
-      var result = this.responseText;
-      console.log(result);
-      var nom_chantier = result["nom_chantier"];
-      var type_cam = result["type_cam"];
-      var resolution = result["resolution"];
-      var avancement = result["avancement"];
-      var nb_etapes = result["nb_etapes"];
-      var statut = result["statut"];
-      var avancement_rel = (avancement/nb_etapes)*100;
+      var result_brut = this.responseText;
+      var result = JSON.parse(result_brut);
 
-    };
+      var nb_chantiers = result.length;
+      console.log(nb_chantiers);
+
+      //On parcourt tous les chantiers trouvés :
+      for(i = 0; i < nb_chantiers; i++){
+
+        //Récupération infos du chantier
+        var nom_chantier = result[i]["nom_chantier"];
+        var type_cam = result[i]["type_cam"];
+        var resolution = result[i]["resolution"];
+        var avancement = result[i]["avancement"];
+        var nb_etapes = result[i]["nb_etapes"];
+        var statut = result[i]["statut"];
+        var avancement_rel = (Number(avancement)/Number(nb_etapes))*100;
+
+        //Affichage dans le DOM
+
+        //On regarde si le chantier est en cours ou terminé pour choisir où on affiche les infos
+        if(statut == "termine"){
+          var div_statut = document.getElementById("termine");
+        }
+        else{
+          var div_statut = document.getElementById("en_cours");
+        }
+
+        var p_chantier = document.createElement("p");
+
+        var texte_nom = "Nom du chantier : "+nom_chantier;
+        var texte_type = "Type de caméra : "+type_cam;
+        var texte_resolution = "Résolution : "+resolution;
+        var texte_avancement = "Avancement : "+avancement_rel+"%";
+
+        var textnode_nom = document.createTextNode(texte_nom);
+        var textnode_type = document.createTextNode(texte_type);
+        var textnode_resolution = document.createTextNode(texte_resolution);
+        var textnode_avancement = document.createTextNode(texte_avancement);
+
+        p_chantier.appendChild(textnode_nom);
+        p_chantier.appendChild(textnode_type);
+        p_chantier.appendChild(textnode_resolution);
+        p_chantier.appendChild(textnode_avancement);
+
+        //Si le chantier est terminé, on propose le lien de téléchargement
+
+        div_statut.appendChild(p_chantier);
+
+      };
+
+      }
+
 	}
 	ajax.open("GET", url, true);
 	ajax.send();
 
 }
 
-function afficherInfos(){
-
-  var div_nom = document.getElementById("nom_chantier");
-  div_nom.innerHTML = "Nom du chantier : "+nom_chantier;
-
-  var div_param = document.getElementById("param");
-  div_param.innerHTML = "Type de caméra : "+type_cam;
-
-  var div_avancement = document.getElementById("avancement");
-  div_avancement.innerHTML = "Avancement : "+avancement_rel;
-
-}
+getInfosChantier();
 
 }
--- mesChantiers.php
+++ mesChantiers.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 //Connexion à la BDD
 $bdd = mysqli_connect("localhost", "root", "", "micmac");
 if (!$bdd) {
@@ -8,17 +10,26 @@ if (!$bdd) {
 }
 
 //Récupération de l'ID de l'user
-$id_user = $_SESSION["id_user"];
+//$id_user = $_SESSION["id_user"];
+$id_user = 1; //provisoire, en attendant de relier avec les autres pages ; ensuite, remettre la ligne au-dessus
 
 //Récupération des informations du chantier
-$requete = "SELECT * FROM chantier WHERE id = ".$id_user;
+$requete = "SELECT * FROM chantiers WHERE id_user = '$id_user'";
 $result = mysqli_query($bdd, $requete);
 if (!$result) {
   echo "Erreur : les informations n'ont pas pu être récupérées.";
   exit;
 }
-$result = mysqli_fetch_assoc($result);
 
-echo $result;
+$tab = array();
+
+while($donnees = mysqli_fetch_assoc($result)){
+  $tab[] = $donnees;
+}
+
+//Conversion en JSON
+$result_json = json_encode($tab);
+
+echo $result_json;
 
  ?>
