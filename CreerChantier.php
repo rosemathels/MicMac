d881688db2c10ee63119cf04e0f5888d06b681e9
RoseMathelier <rose.mathelier@gmail.com>
2017-05-03 12:55:30 +0200

    page création chantier

---
+++ CreerChantier.php
@@ -0,0 +1,26 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>User Page</title>
+    <link rel="stylesheet" href="CreerChantier.css">
+  </head>
+  <body>
+    <div>
+      <form method= "post" action="UploadPicture2.php" enctype="multipart/form-data">
+        <label>Nom du chantier :</label>
+        <input type="text" name="nom_chantier"><br>
+        <label>Type de caméra :</label>
+        <select name="type_camera">
+          <option value="classique" selected>Caméra classique</option>
+          <option value="fisheye">FishEye</option>
+        </select>
+       <input type="file" name="myimage[]" id="myimage" multiple>
+       <input type="submit" name="submit_image" value="Upload"><br>
+     </form>
+    </div>
+  </body>
+</html>
